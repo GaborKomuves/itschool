79df86ccf03fd550f691f5a625752d0b266ad2b0
Gabor Komuves <gabor.komuves@gmail.com>
2024-09-15 18:07:32 +0100

    second

--- challenge_01/Class.java
+++ challenge_01/Class.java
@@ -1,5 +1,7 @@
 public staic class Class{
 	public static boid main(String[] args){
 		System.out.printlin("Lucrez cu git ");
+		System.out.printlin();
+		System.out.printlin("Am facut primul commit");
 	}
 }
